e4569bf0b67600d668da29269559c1da2f055566
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-05-16 15:38:04 +0300

    create-project: add solution and project

---
+++ src/web-dotnet-mentoring/NorthwindWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NorthwindWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
