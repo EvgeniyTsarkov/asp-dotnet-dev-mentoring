253bd11ba38c571a2655769125128396764ec5f5
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-05-16 19:33:51 +0300

    create-project: move services and middleware configs to separate files

---
+++ src/web-dotnet-mentoring/NorthwindWebsite/Configuration/ServicesConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace NorthwindWebsite.Configuration
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection AddServicesConfiguration(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+
+            return services;
+        }
+    }
+}
