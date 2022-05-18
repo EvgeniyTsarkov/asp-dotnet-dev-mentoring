f6ade19a34ec80a554ca36fe8593d9ddf577f0fb
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-05-18 19:45:10 +0300

    task-1-connect-database: refactor NorthwindContext

---
+++ src/NorthwindWebsite/Infrastructure/Configuration/OrderSubtotalConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NorthwindWebsite.Entities;
+
+namespace NorthwindWebsite.Infrastructure.Configuration
+{
+    public class OrderSubtotalConfiguration : IEntityTypeConfiguration<OrdersSubtotal>
+    {
+        public void Configure(EntityTypeBuilder<OrdersSubtotal> builder)
+        {
+            builder.HasNoKey();
+
+            builder.ToView("Order Subtotals");
+
+            builder.Property(e => e.OrderId).HasColumnName("OrderID");
+
+            builder.Property(e => e.Subtotal).HasColumnType("money");
+        }
+    }
+}
---
+++ src/NorthwindWebsite/Infrastructure/Configuration/SummaryOfSalesByQuarterConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NorthwindWebsite.Entities;
+
+namespace NorthwindWebsite.Infrastructure.Configuration
+{
+    public class SummaryOfSalesByQuarterConfiguration : IEntityTypeConfiguration<SummaryOfSalesByQuarter>
+    {
+        public void Configure(EntityTypeBuilder<SummaryOfSalesByQuarter> builder)
+        {
+            builder.HasNoKey();
+
+            builder.ToView("Summary of Sales by Quarter");
+
+            builder.Property(e => e.OrderId).HasColumnName("OrderID");
+
+            builder.Property(e => e.ShippedDate).HasColumnType("datetime");
+
+            builder.Property(e => e.Subtotal).HasColumnType("money");
+        }
+    }
+}
