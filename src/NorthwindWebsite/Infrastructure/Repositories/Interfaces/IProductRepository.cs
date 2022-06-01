df48bdf546ba12c5ab18aeb08f43552cd66cc548
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-06-01 20:23:47 +0300

    Task-2-2-action-results-and-file-uploads: uncomment maxfilelimits attribute

--- src/NorthwindWebsite/Infrastructure/Repositories/Interfaces/IProductRepository.cs
+++ src/NorthwindWebsite/Infrastructure/Repositories/Interfaces/IProductRepository.cs
@@ -2,10 +2,8 @@
 
 namespace NorthwindWebsite.Infrastructure.Repositories.Interfaces;
 
-public interface IProductRepository
+public interface IProductRepository : IBaseRepository<Product>
 {
-    Task<List<Product>> GetAll();
-
     Task<List<Product>> GetLimitedNumberOfProducts(int limit);
 
     Task<Product> Get(int id, bool skipRelatedItems = false);
