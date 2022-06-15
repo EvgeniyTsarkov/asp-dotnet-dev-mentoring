b04634af2effc1a38a58672f4ad07a54ae0ff113
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-06-15 21:03:35 +0300

    Task-2-components-partials: add breadcrumps

---
+++ src/NorthwindWebsite/Presentation/Views/Shared/Components/Breadcrumps/Breadcrumps.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace NorthwindWebsite.Presentation.Views.Shared.Components.Breadcrumps;
+
+public class Breadcrumps : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var path = HttpContext.Request.Path.ToString();
+
+        var breadcrumps = new StringBuilder("Home");
+
+        var processedPath = ProcessSpecialCases(path);
+
+        breadcrumps.Append(processedPath.Replace("/", " > "));
+
+        return View("_Breadcrumps", breadcrumps);
+    }
+
+    private static StringBuilder ProcessSpecialCases(string path)
+    {
+        var processedPath = new StringBuilder(string.Empty);
+
+        if (path.Contains("Handle"))
+        {
+            if (path.EndsWith("Handle"))
+            {
+                processedPath.Append(path.Replace("Handle", "Create New"));
+            }
+            else
+            {
+                var pathElements = path.Split("Handle");
+                processedPath.Append(pathElements[0] + "Edit");
+            }
+        }
+        else if (path.Contains("ImageUpload"))
+        {
+            var pathElements = path.Split("ImageUpload");
+            processedPath.Append(pathElements[0] + "Image Upload");
+        }
+        else
+        {
+            processedPath.Append(path);
+        }
+
+
+        if (path.Contains("Home"))
+        {
+            processedPath.Replace("Home/", string.Empty);
+        }
+
+        return processedPath;
+    }
+}
