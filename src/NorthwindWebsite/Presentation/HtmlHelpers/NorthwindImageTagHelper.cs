f25860c5549bb56642c87493b56fc99938238996
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-06-18 19:58:23 +0300

    Task-2-html-helpers-and-tags: add link text attribute

--- src/NorthwindWebsite/Presentation/HtmlHelpers/NorthwindImageTagHelper.cs
+++ src/NorthwindWebsite/Presentation/HtmlHelpers/NorthwindImageTagHelper.cs
@@ -5,14 +5,23 @@ namespace NorthwindWebsite.Presentation.HtmlHelpers;
 [HtmlTargetElement("northwind-image", TagStructure = TagStructure.NormalOrSelfClosing)]
 public class NorthwindImageTagHelper : TagHelper
 {
+    private const string ImageUploadLinkPatern = "/Categories/ImageUpload/";
+
     public int Id { get; set; }
+
+    public string Text { get; set; }
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = "a";
 
-        var uploadImageLink = "/Categories/ImageUpload/" + Id;
+        var uploadImageLink = string.Concat(ImageUploadLinkPatern, Id);
 
         output.Attributes.SetAttribute("href", uploadImageLink);
+
+        if (Text != null)
+        {
+            output.Attributes.SetAttribute("title", Text);
+        }
     }
 }
--- src/NorthwindWebsite/Presentation/HtmlTags/NorthwindImageExtensions.cs
+++ src/NorthwindWebsite/Presentation/HtmlTags/NorthwindImageExtensions.cs
@@ -5,11 +5,11 @@ namespace NorthwindWebsite.Presentation.HtmlHelpers;
 
 public static class NorthwindImageExtensions
 {
+    private const string ImageLinkPattern = "<img src=\"/Images/{0}\" width=\"{1}\", height=\"{2}\" type=\"{3}\"/>";
+
     public static HtmlString NorthwindImageLink(this IHtmlHelper helper, int id, int width, double height, string imageType)
     {
-        string code = string.Format(
-            "<img src=\"/Images/{0}\" width=\"{1}\", height=\"{2}\" type=\"{3}\"/>",
-            id, width, height, imageType);
+        var code = string.Format(ImageLinkPattern, id, width, height, imageType);
 
         return new HtmlString(code);
     }
