6d2de6f621b26e8b19d356c437029115f24bb7d8
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-06-18 11:56:15 +0300

    remove typos and introduce constants for action names

--- src/NorthwindWebsite/Presentation/Views/Shared/Components/Breadcrumbs/Breadcrumbs.cs
+++ src/NorthwindWebsite/Presentation/Views/Shared/Components/Breadcrumbs/Breadcrumbs.cs
@@ -3,40 +3,44 @@ using System.Text;
 
 namespace NorthwindWebsite.Presentation.Views.Shared.Components.Breadcrumps;
 
-public class Breadcrumps : ViewComponent
+public class Breadcrumbs : ViewComponent
 {
+    private const string NavigationStartPoint = "Home";
+    private const string HandleActionName = "Handle";
+    private const string ImageUploadActionName = "ImageUpload";
+
     public IViewComponentResult Invoke()
     {
         var path = HttpContext.Request.Path.ToString();
 
-        var breadcrumps = new StringBuilder("Home");
+        var breadcrumps = new StringBuilder(NavigationStartPoint);
 
         var processedPath = ProcessSpecialCases(path);
 
         breadcrumps.Append(processedPath.Replace("/", " > "));
 
-        return View("_Breadcrumps", breadcrumps);
+        return View("_Breadcrumbs", breadcrumps);
     }
 
     private static StringBuilder ProcessSpecialCases(string path)
     {
         var processedPath = new StringBuilder(string.Empty);
 
-        if (path.Contains("Handle"))
+        if (path.Contains(HandleActionName))
         {
-            if (path.EndsWith("Handle"))
+            if (path.EndsWith(HandleActionName))
             {
-                processedPath.Append(path.Replace("Handle", "Create New"));
+                processedPath.Append(path.Replace(HandleActionName, "Create New"));
             }
             else
             {
-                var pathElements = path.Split("Handle");
+                var pathElements = path.Split(HandleActionName);
                 processedPath.Append(pathElements[0] + "Edit");
             }
         }
-        else if (path.Contains("ImageUpload"))
+        else if (path.Contains(ImageUploadActionName))
         {
-            var pathElements = path.Split("ImageUpload");
+            var pathElements = path.Split(ImageUploadActionName);
             processedPath.Append(pathElements[0] + "Image Upload");
         }
         else
@@ -44,8 +48,7 @@ public class Breadcrumps : ViewComponent
             processedPath.Append(path);
         }
 
-
-        if (path.Contains("Home"))
+        if (path.Contains(NavigationStartPoint))
         {
             processedPath.Replace("Home/", string.Empty);
         }
