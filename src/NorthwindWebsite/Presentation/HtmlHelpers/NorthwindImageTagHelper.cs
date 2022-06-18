c894381677b287870a5d50ebb85cc02d89c1c598
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-06-18 19:02:26 +0300

    Task-2-html-helpers-and-tags: add helper and tag

---
+++ src/NorthwindWebsite/Presentation/HtmlHelpers/NorthwindImageTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NorthwindWebsite.Presentation.HtmlHelpers;
+
+[HtmlTargetElement("northwind-image", TagStructure = TagStructure.NormalOrSelfClosing)]
+public class NorthwindImageTagHelper : TagHelper
+{
+    public int Id { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "a";
+
+        var uploadImageLink = "/Categories/ImageUpload/" + Id;
+
+        output.Attributes.SetAttribute("href", uploadImageLink);
+    }
+}
---
+++ src/NorthwindWebsite/Presentation/HtmlTags/NorthwindImageExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NorthwindWebsite.Presentation.HtmlHelpers;
+
+public static class NorthwindImageExtensions
+{
+    public static HtmlString NorthwindImageLink(this IHtmlHelper helper, int id, int width, double height, string imageType)
+    {
+        string code = string.Format(
+            "<img src=\"/Images/{0}\" width=\"{1}\", height=\"{2}\" type=\"{3}\"/>",
+            id, width, height, imageType);
+
+        return new HtmlString(code);
+    }
+}
