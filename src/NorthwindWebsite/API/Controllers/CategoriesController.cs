c855d0bf3f7eff0aba806769882973c844409e65
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2022-07-17 19:45:34 +0300

    Add API controller methods description

--- src/NorthwindWebsite/API/Controllers/CategoriesController.cs
+++ src/NorthwindWebsite/API/Controllers/CategoriesController.cs
@@ -18,6 +18,10 @@ public class CategoriesController : ControllerBase
         _categoryService = categoryService;
     }
 
+    /// <summary>
+    /// Returns all categories from database.
+    /// </summary>
+    /// <returns><see cref="Task">Representing an asynchronous operarion.</returns>
     [HttpGet(Name = nameof(GetCategories))]
     public async Task<ActionResult<List<Category>>> GetCategories()
     {
@@ -26,6 +30,12 @@ public class CategoriesController : ControllerBase
         return categories.ToList();
     }
 
+    /// <summary>
+    /// Returns and image for a category by id.
+    /// </summary>
+    /// <param name="categoryId">Id of the category that contains the image.</param>
+    /// <response code="404">Unable to find a category with the mentioned id.</response>
+    /// <returns><see cref="Task">Represents an asynchronous operation.</returns>
     [HttpGet("id/{categoryId:int}/image")]
     [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
     public async Task<ActionResult> GetImage(int categoryId)
@@ -40,6 +50,13 @@ public class CategoriesController : ControllerBase
         return File(picture, FileFormat);
     }
 
+    /// <summary>
+    /// Saves mthe image to the database for the category with the specified id.
+    /// </summary>
+    /// <param name="categoryId">Id of the category the image will be saved to.</param>
+    /// <param name="imageDto"><see cref="ImageDto"></param>
+    /// <response code="404">Unable to find a category with the mentioned id.</response>
+    /// <returns><see cref="Task">Represents an asynchronous operation.</returns>
     [HttpPut("id/{categoryId:int}/image")]
     [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
     public async Task<ActionResult> UploadImage(
